af955f28b9525e1168f9959e78bd315785602d0a
vPrah <165430229+vPrah@users.noreply.github.com>
2024-05-14 07:52:34 -0400

    Removed unused TimeUtil util

--- src/main/java/cc/unknown/module/impl/other/AutoRegister.java
+++ src/main/java/cc/unknown/module/impl/other/AutoRegister.java
@@ -6,7 +6,7 @@ import cc.unknown.event.impl.player.TickEvent;
 import cc.unknown.module.impl.Module;
 import cc.unknown.module.impl.api.Category;
 import cc.unknown.module.impl.api.Register;
-import cc.unknown.utils.player.TimeUtil;
+import cc.unknown.utils.client.Cold;
 import net.minecraft.network.Packet;
 import net.minecraft.network.play.server.S02PacketChat;
 import org.apache.commons.lang3.StringUtils;
@@ -15,10 +15,10 @@ import org.apache.commons.lang3.StringUtils;
 public class AutoRegister extends Module {
 
     private String text;
-    private final TimeUtil timeUtil;
+    private final Cold cold;
 
     public AutoRegister() {
-        timeUtil = new TimeUtil();
+        cold = new Cold();
     }
 
     @EventLink
@@ -29,18 +29,18 @@ public class AutoRegister extends Module {
             String text = s02PacketChat.getChatComponent().getUnformattedText();
             if (StringUtils.containsIgnoreCase(text, "/register") || StringUtils.containsIgnoreCase(text, "/register password password") || text.equalsIgnoreCase("/register <password> <password>")) {
                 this.text = "/register DglaMaska13 DglaMaska13";
-                timeUtil.reset();
+                cold.reset();
             }
             else if (StringUtils.containsIgnoreCase(text, "/login password") || StringUtils.containsIgnoreCase(text, "/login") || text.equalsIgnoreCase("/login <password>")) {
                 this.text = "/login DglaMaska13";
-                timeUtil.reset();
+                cold.reset();
             }
         }
     }
 
     @EventLink
     public void onTick (TickEvent event) {
-        if (timeUtil.hasReached(1500L) && text != null && !text.equals("")) {
+        if (cold.reached(1500L) && text != null && !text.equals("")) {
             mc.thePlayer.sendChatMessage(text);
             System.out.println(text);
             text = "";
